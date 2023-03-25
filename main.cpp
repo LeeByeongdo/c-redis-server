0b3e26fb4b8ec8e5b93f4f733e2142f0eee1a9e6
byeongdolee <bdbdbdlee@gmail.com>
2023-03-25 17:49:18 +0900

    ch7 완료

--- main.cpp
+++ main.cpp
@@ -6,9 +6,11 @@
 #include <cstring>
 #include <netinet/in.h>
 #include <cassert>
+#include <string>
 #include <vector>
 #include <sys/fcntl.h>
 #include <sys/poll.h>
+#include <map>
 
 static void msg(const char *msg) {
     fprintf(stderr, "%s\n", msg);
@@ -20,74 +22,8 @@ static void die(const char *msg) {
     abort();
 }
 
-static int32_t read_full(int fd, char *buf, size_t n) {
-    while (n > 0) {
-        ssize_t rv = read(fd, buf, n);
-        if (rv <= 0) {
-            return -1;
-        }
-
-        assert((size_t) rv <= n);
-        n -= (size_t) rv;
-        buf += rv;
-    }
-    return 0;
-}
-
-static int32_t write_all(int fd, const char *buf, size_t n) {
-    while (n > 0) {
-        ssize_t rv = write(fd, buf, n);
-        if (rv <= 0) {
-            return -1;
-        }
-
-        assert((size_t) rv <= n);
-        n -= (size_t) rv;
-        buf += rv;
-    }
-    return 0;
-}
-
 const size_t k_max_msg = 4096;
 
-static int32_t one_request(int connfd) {
-    // 4bytes header
-    char rbuf[4 + k_max_msg + 1];
-    errno = 0;
-    int32_t err = read_full(connfd, rbuf, 4);
-    if (err) {
-        if (errno == 0) {
-            msg("EOF");
-        } else {
-            msg("read() error");
-        }
-        return err;
-    }
-
-    uint32_t len = 0;
-    memcpy(&len, rbuf, 4);
-    if (len > k_max_msg) {
-        msg("too long");
-        return -1;
-    }
-
-    err = read_full(connfd, &rbuf[4], len);
-    if (err) {
-        msg("read() error");
-        return err;
-    }
-
-    rbuf[4 + len] = '\0';
-    printf("client says: %s\n", &rbuf[4]);
-
-    // reply
-    const char reply[] = "world";
-    char wbuf[4 + sizeof(reply)];
-    len = (uint32_t) strlen(reply);
-    memcpy(wbuf, &len, 4);
-    memcpy(&wbuf[4], reply, len);
-    return write_all(connfd, wbuf, 4 + len);
-}
 
 enum {
     STATE_REQ = 0,
@@ -183,8 +119,100 @@ static bool try_fill_buffer(Conn *conn) {
     return (conn->state == STATE_REQ);
 }
 
+const size_t k_max_args = 1024;
+
+static int32_t parse_req(const uint8_t *data, size_t len, std::vector<std::string> &out) {
+    if (len < 4) {
+        return -1;
+    }
+
+    uint32_t n = 0;
+    memcpy(&n, &data[0], 4);
+    if (n > k_max_args) {
+        return -1;
+    }
+
+    size_t pos = 4;
+    while (n--) {
+        if (pos + 4 > len) {
+            return -1;
+        }
+        uint32_t sz = 0;
+        memcpy(&sz, &data[pos], 4);
+        if (pos + 4 + sz > len) {
+            return -1;
+        }
+
+        out.push_back(std::string((char *) &data[pos + 4], sz));
+        pos += 4 + sz;
+    }
+
+    if (pos != len) {
+        return -1;
+    }
+
+    return 0;
+}
+
+enum {
+    RES_OK = 0,
+    RES_ERR = 1,
+    RES_NX = 2,
+};
+
+static std::map<std::string, std::string> g_map;
+
+static uint32_t do_get(const std::vector<std::string> &cmd, uint8_t *res, uint32_t *reslen) {
+    if (!g_map.count(cmd[1])) {
+        return RES_NX;
+    }
+
+    std::string &val = g_map[cmd[1]];
+    assert(val.size() <= k_max_msg);
+    memcpy(res, val.data(), val.size());
+    *reslen = (uint32_t) val.size();
+    return RES_OK;
+}
+
+static uint32_t do_set(const std::vector<std::string> &cmd, uint8_t *res, uint32_t *reslen) {
+    (void) res;
+    (void) reslen;
+    g_map[cmd[1]] = cmd[2];
+    return RES_OK;
+}
+
+static uint32_t do_del(const std::vector<std::string> &cmd, uint8_t *res, uint32_t *reslen) {
+    (void) res;
+    (void) reslen;
+    g_map.erase(cmd[1]);
+    return RES_OK;
+}
+
+static bool cmd_is(const std::string &word, const char *cmd) {
+    return 0 == strcasecmp(word.c_str(), cmd);
+}
+
 static int32_t do_request(const uint8_t *req, uint32_t reqlen, uint32_t *rescode, uint8_t *res, uint32_t *reslen) {
-    
+    std::vector<std::string> cmd;
+    if (0 != parse_req(req, reqlen, cmd)) {
+        msg("bad req");
+        return -1;
+    }
+
+    if (cmd.size() == 2 && cmd_is(cmd[0], "get")) {
+        *rescode = do_get(cmd, res, reslen);
+    } else if (cmd.size() == 3 && cmd_is(cmd[0], "set")) {
+        *rescode = do_set(cmd, res, reslen);
+    } else if (cmd.size() == 2 && cmd_is(cmd[0], "del")) {
+        *rescode = do_del(cmd, res, reslen);
+    } else {
+        *rescode = RES_ERR;
+        const char *msg = "Unknown cmd";
+        strcpy((char *) res, msg);
+        *reslen = strlen(msg);
+        return 0;
+    }
+    return 0;
 }
 
 static bool try_one_request(Conn *conn) {
@@ -306,7 +334,7 @@ int main() {
 
     struct sockaddr_in addr = {};
     addr.sin_family = AF_INET;
-    addr.sin_port = ntohs(8080);
+    addr.sin_port = ntohs(8085);
     addr.sin_addr.s_addr = ntohl(0);
     int rv = bind(fd, (const sockaddr *) &addr, sizeof(addr));
     if (rv) {
